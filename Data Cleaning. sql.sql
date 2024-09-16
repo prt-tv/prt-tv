780edc63d166352ba465fce81aa415b0609b9f83
Priti Dabhi <pritydabhi02@gmail.com>
2024-09-16 13:54:44 +0530

    Update Data Cleaning. sql.sql

--- Data Cleaning. sql.sql
+++ Data Cleaning. sql.sql
@@ -1,4 +1,4 @@
--- Data Cleaning
+-- SQL Project-- Data Cleaning
 
 SELECT *
 FROM layoffs;
@@ -73,6 +73,7 @@ DELETE
 FROM layoffs_staging2
 WHERE row_num > 1;
 
+-- -----------------------------------------------------------------------------------------------------------------------------------------------
 
 -- 2. Standerdize Data
 
@@ -85,6 +86,8 @@ FROM layoffs_staging2;
 UPDATE layoffs_staging2
 SET company = TRIM(company);
 
+-- I also noticed the Crypto has multiple different variations. WE need to standardize that - let's say all to Crypto
+
 SELECT DISTINCT industry
 FROM layoffs_staging2
 WHERE industry LIKE 'Crypto%';
@@ -93,19 +96,20 @@ UPDATE layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry LIKE 'Crypto%';
 
+
+-- Let's fix the date column
 SELECT `date` ,STR_TO_DATE(`date` , '%m/%d/%Y')
 FROM layoffs_staging2;
 
-ALTER TABLE layoffs_staging2
-MODIFY COLUMN `date` DATE;
-
+-- we  can use str to date to update this field
 UPDATE layoffs_staging2
 SET `date` = STR_TO_DATE(`date` , '%m/%d/%Y');
 
+-- now we can convert the data type properly
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
 
-SELECT DISTINCT country
-FROM layoffs_staging2
-ORDER BY 1 ;
+-- We have some  "United States" and some "United states." with a period at the end. Let's standerdize this
 
 SELECT DISTINCT COUNTRY
 FROM layoffs_staging2
@@ -119,32 +123,46 @@ UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country)
 WHERE country LIKE 'United States%';
 
+-- now if we run this again it is fixed
 SELECT DISTINCT country
 FROM layoffs_staging2
 ORDER BY 1;
 
-  -- if we look at industry is look  like we some null and empty rows
+  -- if we look at industry is look  like we some null and empty rows, let's take a look at these
+
 SELECT  DISTINCT industry
 FROM layoffs_staging2
 WHERE industry IS NULL
 AND industry = '';
 
-SELECT DISTINCT industry
-FROM layoffs_staging2
-ORDER BY 1;
-
-SELECT DISTINCT industry
-FROM layoffs_staging2
-ORDER BY 1;
+-- let's take a look at these
 
 SELECT *
-FROM layoffs_staging
-WHERE industry IS NULL
-OR industry = '';
+FROM layoffs_sstaging2
+WHERE company LIKE 'Bally%';
+
+-- nothing wrong here 
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'airbnb%';
+
+-- it look like airbnb is travel, but this one just isn't populated
+--write a query that if there is another row with the same company name, it will update it to the non-null industry values
+--make it easy so if there were thousands we wound't have manually check them all
+
+-- we should set blanks to nulls since those are typically easser to work with
 
 UPDATE layoffs_staging2
 SET industry = null
 WHERE industry = '';
+
+-- now if we check those are all null
+SELECT *
+FROM layoffs_staging
+WHERE industry IS NULL
+OR industry = ''
+ORDER BY industry;
 
 SELECT *
 FROM layoffs_staging2
@@ -157,6 +175,8 @@ ON t1.company = t2.company
 WHERE t1.industry IS NULL 
 AND t2.industry IS NOT NULL;
 
+-- now we need to pupulate those null if possible 
+
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
 ON t1.company = t2.company
@@ -164,11 +184,30 @@ SET t1.industry = t2.industry
 WHERE t1.industry IS NULL 
 AND t2.industry IS NOT NULL;
 
+-- and if we check it looks Bally's was the only one without populated row to populate this null values
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = ''
+ORDER BY industry;
+
+
+-- 3. Look at null Values
+-- the null values in total_laid_off, percentage_laid_off and funds_raised_millions all look normal. i don't think i want to change that
+-- I like having them null because it make it eassier for calculation during the EDA phase
+
+-- so there isn't anything i want to change with null values
+
+
+-- 4 Remove any column and rows we need to
+
 SELECT *
 FROM layoffs_staging2
 WHERE total_laid_off is NULL
 AND percentage_laid_off IS NULL;
 
+-- Delete Uselase data we can't really use 
 DELETE 
 FROM layoffs_staging2
 WHERE total_laid_off is NULL
